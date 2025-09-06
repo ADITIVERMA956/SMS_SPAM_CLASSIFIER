ddc2e257397b4be1ae8353e8b62454f1f015730b
ADITIVERMA956 <aditivns9569@gmail.com>
2025-09-06 23:48:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,47 +2,50 @@ import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 import nltk
-
-# Download stopwords only
-@st.cache_data(show_spinner=False)
-def download_stopwords():
-    nltk.download('stopwords')
-
-download_stopwords()
+from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
+
 def transform_text(text):
     text = text.lower()
-    text = text.split()  # Simple tokenizer without nltk.word_tokenize
+    text = nltk.word_tokenize(text)
 
-    # Keep only alphanumeric words
-    y = [word for word in text if word.isalnum()]
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
 
-    # Remove stopwords and punctuation
-    y = [word for word in y if word not in stopwords.words('english') and word not in string.punctuation]
+    text = y[:]
+    y.clear()
 
-    # Stemming
-    y = [ps.stem(word) for word in y]
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
 
     return " ".join(y)
 
-# Load your pickled model and vectorizer (with exact filenames)
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+tfidf = pickle.load(open('vectorizer .pkl','rb'))
+model = pickle.load(open('model .pkl','rb'))
 
-st.title("SMS Spam Classifier")
+st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-    # 1. Preprocess
+
+    # 1. preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. Vectorize
+    # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. Predict
+    # 3. predict
     result = model.predict(vector_input)[0]
     # 4. Display
     if result == 1:
